b50201004070a2109edd9a51f40bdbd88b66e266
james kariuki <103502270+Kkigathi@users.noreply.github.com>
2022-09-07 19:15:00 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,4 +1,4 @@
-ifndef _SHELL_H_
+#ifndef _SHELL_H_
 #define _SHELL_H_
 
 /*
@@ -137,4 +137,4 @@ void help_unsetenv(void);
 void help_history(void);
 
 int proc_file_commands(char *file_path, int *exe_ret);
-#endif /* _SHELL_H_ */
+#endif
