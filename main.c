815d09b7f29c89b52b283eee67c24a56b6faaef9
james kariuki <103502270+Kkigathi@users.noreply.github.com>
2022-09-07 08:52:45 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-
-#include "shell.h"
-void sig_handler(int sig);
-int execute(char **args, char **front);
-=======
 #include "shell.h"
 
 void sig_handler(int sig);
 int execute(char **args, char **front);
 
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 /**
  * sig_handler - Prints a new prompt upon a signal.
  * @sig: The signal.
@@ -17,18 +10,12 @@ int execute(char **args, char **front);
 void sig_handler(int sig)
 {
 	char *new_prompt = "\n$ ";
-<<<<<<< HEAD
-=======
 
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 	(void)sig;
 	signal(SIGINT, sig_handler);
 	write(STDIN_FILENO, new_prompt, 3);
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 /**
  * execute - Executes a command in a child process.
  * @args: An array of arguments.
@@ -42,33 +29,12 @@ int execute(char **args, char **front)
 	pid_t child_pid;
 	int status, flag = 0, ret = 0;
 	char *command = args[0];
-<<<<<<< HEAD
-=======
 
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 	if (command[0] != '/' && command[0] != '.')
 	{
 		flag = 1;
 		command = get_location(command);
 	}
-<<<<<<< HEAD
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		if (flag)
-			free(command);
-		perror("Error child:");
-		return (1);
-	}
-	if (child_pid == 0)
-	{
-		if (!command || (access(command, F_OK) == -1))
-		{
-			if (errno == EACCES)
-				ret = (create_error(args, 126));
-			else
-				ret = (create_error(args, 127));
-=======
 
 	if (!command || (access(command, F_OK) == -1))
 	{
@@ -92,45 +58,22 @@ int execute(char **args, char **front)
 			execve(command, args, environ);
 			if (errno == EACCES)
 				ret = (create_error(args, 126));
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 			free_env();
 			free_args(args, front);
 			free_alias_list(aliases);
 			_exit(ret);
 		}
-<<<<<<< HEAD
-	/*
-	*	if (access(command, X_OK) == -1)
-	*		return (create_error(argv[0], 126));
-	*/
-		execve(command, args, NULL);
-		if (errno == EACCES)
-			ret = (create_error(args, 126));
-		free_env();
-		free_args(args, front);
-		free_alias_list(aliases);
-		_exit(ret);
-	}
-	else
-	{
-		wait(&status);
-		ret = WEXITSTATUS(status);
-=======
 		else
 		{
 			wait(&status);
 			ret = WEXITSTATUS(status);
 		}
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 	}
 	if (flag)
 		free(command);
 	return (ret);
 }
-<<<<<<< HEAD
-=======
 
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 /**
  * main - Runs a simple UNIX command interpreter.
  * @argc: The number of arguments supplied to the program.
@@ -143,18 +86,12 @@ int main(int argc, char *argv[])
 	int ret = 0, retn;
 	int *exe_ret = &retn;
 	char *prompt = "$ ", *new_line = "\n";
-<<<<<<< HEAD
-=======
 
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 	name = argv[0];
 	hist = 1;
 	aliases = NULL;
 	signal(SIGINT, sig_handler);
-<<<<<<< HEAD
-=======
 
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 	*exe_ret = 0;
 	environ = _copyenv();
 	if (!environ)
@@ -162,15 +99,8 @@ int main(int argc, char *argv[])
 
 	if (argc != 1)
 	{
-<<<<<<< HEAD
-		ret = proc_file_commands(argv[1]);
 		ret = proc_file_commands(argv[1], exe_ret);
 		free_env();
-		return (ret);
-=======
-		ret = proc_file_commands(argv[1], exe_ret);
-		free_env();
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 		free_alias_list(aliases);
 		return (*exe_ret);
 	}
@@ -183,10 +113,7 @@ int main(int argc, char *argv[])
 		free_alias_list(aliases);
 		return (*exe_ret);
 	}
-<<<<<<< HEAD
-=======
 
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
 	while (1)
 	{
 		write(STDOUT_FILENO, prompt, 2);
@@ -200,15 +127,8 @@ int main(int argc, char *argv[])
 			exit(*exe_ret);
 		}
 	}
-<<<<<<< HEAD
-	free_env();
-	free_alias_list(aliases);
-	return (*exe_ret);
-}
-=======
 
 	free_env();
 	free_alias_list(aliases);
 	return (*exe_ret);
 }
->>>>>>> fef356ee3dc37ba9b72471e051a4dceccc693238
